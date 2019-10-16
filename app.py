6ba75089a04ec95a4f6676e9af61c51de797ef5d
Debankur Saha <debankurs@pop-os.localdomain>
2019-10-16 16:39:29 +0530

    Updated CORS config

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template,request, Response
 from flask_restful import Resource, Api
-
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 api=Api(app)
 	
 
